944469f3d013f136ef9ddef0ba5a1eec7bdb0e14
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-15 05:20:38 +0500

    Task3

---
+++ Tyuiu.FendelNS.Sprint5.Task3.V26.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.FendelNS.Sprint5.Task3.V26.Lib
+{
+    public class DataService : ISprint5Task3V26
+    {
+
+        public string SaveToFileTextData(int x)
+        {
+            double calculation = Math.Round((0.7 * (Math.Pow(x, 3))) + (1.52 * (Math.Pow(x, 2))), 3);
+            byte[] byteArray = BitConverter.GetBytes(calculation);
+            string tempFilePath = Path.GetTempFileName();
+            File.WriteAllBytes(tempFilePath, byteArray);
+            return tempFilePath;
+        }
+    }
+}

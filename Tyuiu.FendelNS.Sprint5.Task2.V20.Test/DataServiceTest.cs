26e21e78c772ea88a27ae8e66da8b144b572a7d6
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-15 05:15:17 +0500

    Task2

---
+++ Tyuiu.FendelNS.Sprint5.Task2.V20.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.FendelNS.Sprint5.Task2.V20.Lib;
+namespace Tyuiu.FendelNS.Sprint5.Task2.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(fileExists, wait);
+        }
+    }
+}
---
+++ Tyuiu.FendelNS.Sprint5.Task2.V20/Program.cs
@@ -0,0 +1,56 @@
+﻿using Tyuiu.FendelNS.Sprint5.Task2.V20.Lib;
+namespace Tyuiu.FendelNS.Sprint5.Task2.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Фендель Н. С. | СМАРТБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Фендель Николай Сергеевич | СМАРТБ-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [6, 15] сумму всех делителей                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] mtrx = new int[3, 3] { { 5, -5, -1 },
+                                          {-4, 2, -4 },
+                                          { -7, 1, 4 }};
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+
+            Console.WriteLine("массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(mtrx);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}

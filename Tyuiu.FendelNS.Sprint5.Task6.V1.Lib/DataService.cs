68341a29b7636b3bfb2640a6048833288b5dccfd
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-15 06:37:25 +0500

    Task6

---
+++ Tyuiu.FendelNS.Sprint5.Task6.V1.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.FendelNS.Sprint5.Task6.V1.Lib
+{
+    public class DataService : ISprint5Task6V1
+    {
+        public double CalculateSumOfPrimeIntegersFromFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int LoadFromDataFile(string path)
+        {
+            int res = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    foreach (char i in line)
+                    {
+                        if (char.IsDigit(i))
+                        {
+                            res++;
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.FendelNS.Sprint5.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.FendelNS.Sprint5.Task6.V1.Lib;
+namespace Tyuiu.FendelNS.Sprint5.Task6.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask6V1.txt";
+
+
+            FileInfo fl = new FileInfo(path);
+            bool fileExists = fl.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask6V1.txt";
+            int res = ds.LoadFromDataFile(path);
+            int wait = 6;
+        }
+    }
+}
